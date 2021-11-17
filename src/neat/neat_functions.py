9b47a91989841a724849f3088eb2e76ed1428beb
dipplestix <chrism315@gmail.com>
2021-11-17 04:12:34 -0500

    First draft of breed function. Some restructuring of repo and changes to base structures.

---
+++ src/neat/neat_functions.py
@@ -0,0 +1,44 @@
+from neat.neat_structures import Genome, Gene, Species
+from copy import deepcopy
+from random import random, choice
+from typing import Callable
+
+
+def breed(g1: Genome, g2: Genome, get_fitness: Callable) -> Genome:
+    """
+    :param g1: Genome of the first parent
+    :param g2: Genome of the second parent
+    :param fitness: Genome of the second parent
+    :return: Genome of the child
+    """
+    if g1 > g2:
+        better_parent = g1
+        other_parent = g2
+    else:
+        better_parent = g2
+        other_parent = g1
+    genome_dic = deepcopy(better_parent.ino_dic)
+    for ino in genome_dic:
+        if ino in other_parent.inos:
+            if not better_parent.ino_dic[ino].active or not other_parent.ino_dic[ino].active:
+                if random < .75:
+                    genome_dic[ino].active = False
+                else:
+                    genome_dic[ino].active = True
+            genome_dic[ino].w = choice([better_parent.ino_dic[ino].w, other_parent.ino_dic[ino].w])
+
+    genome = list(genome_dic.values())
+
+    fitness = get_fitness(genome)
+
+    child = Genome(genome, fitness)
+
+    return child
+
+
+
+
+
+
+
+
---
+++ src/neat/neat_structures.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+class Genome:
+    def __init__(self, genes: List, fitness: float):
+        self.genes = genes
+        self.fitness = fitness
+        self.inos = set([g.ino for g in genes])
+        self.ino_dic = {g.ino: g for g in genes}
+
+    def __gt__(self, other):
+        return self.fitness >= other.fitness
+
+    def __lt__(self, other):
+        return self.fitness < other.fitness
+
+    def __eq__(self, other):
+        return self.fitness == other.fitness
+
+
+class Gene:
+    def __init__(self, n_in: int, n_out: int, w: float, ino: int, active: bool):
+        self.n_in = n_in
+        self.n_out = n_out
+        self.w = w
+        self.ino = ino
+        self.active = active
+
+    def __str__(self):
+        if self.active:
+            active_stat = 'active'
+        else:
+            active_stat = 'disabled'
+        return f'Connection between {self.n_in} to {self.n_out} has weight {self.w} and innovation number {self.ino}' \
+               f'and is {active_stat}.'
+
+
+class Species:
+    def __init__(self, genomes: List, champion: Genome):
+        self.genomes = genomes
+        self.champion = champion
